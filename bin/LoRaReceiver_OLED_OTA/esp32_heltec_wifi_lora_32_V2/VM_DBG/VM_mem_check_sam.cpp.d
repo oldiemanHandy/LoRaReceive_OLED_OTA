41fdc201760d44350daac8ce8756a38575ca8e00
Oldieman <mail@shees.de>
2025-10-08 01:01:05 +0200

    Mit gitignore

---
+++ bin/LoRaReceiver_OLED_OTA/esp32_heltec_wifi_lora_32_V2/VM_DBG/VM_mem_check_sam.cpp.d
@@ -0,0 +1,2 @@
+L:\\ESP32\\LoRaReceiver_OLED_OTA\\bin\\LoRaReceiver_OLED_OTA\\esp32_heltec_wifi_lora_32_V2\\VM_DBG\\VM_mem_check_sam.cpp.o: \
+ c:\program\ files\microsoft\ visual\ studio\2022\community\common7\ide\extensions\1k0ittsa.sce\micro\ platforms\default\debuggers\vm_dbg\vm_mem_check_sam.cpp
